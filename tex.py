252aebfe4b319a2d1890541e37a5f8ae255796ac
Stephen Rosen <sirosen@uchicago.edu>
2012-11-06 14:37:55 -0600

    Added support for arbitrary lists of values to the table printing.

--- tex.py
+++ tex.py
@@ -1,19 +1,32 @@
 from math import *
 
-def makeRow(l):
-  return " & ".join(map(lambda x: str(x),l)) + "\\\\\n"
+def makeRow(num,row):
+  return num + " & " + row + "\\\\\n"
 
 def ztable(l,s):
-  body = map(lambda x: makeRow(list(x)), zip(range(1,1 + len(l)), l))
-  table = '\\begin{tabular}{|c|c|}\n' + '\\hline\n' + 'Trial & ' + s + '\n' + '\\hline\n' + ''.join(body) + '\\hline\n' + '\\end{tabular}\n'
+  body = [makeRow(str(x),str(y)) for (x,y) in zip(range(1,1 + len(l)), l)]
+  numcols = 0
+  if len(body) > 0: numcols = len(body[0].split('&'))
+  if numcols == 0: return ''
+
+  table = '\\begin{tabular}{|' + (numcols*'c|') + '}\n' + \
+          '\\hline\n' + \
+          'Trial & ' + s + '\n' + \
+          '\\hline\n' + \
+          ''.join(body) + \
+          '\\hline\n' + \
+          '\\end{tabular}\n'
   return table
+
+def ztable_list(ls,s):
+    return ztable([' & '.join(l) for l in ls],' & '.join(s))
 
 def mean(l):
   return (1.0*sum(l))/len(l)
 
 def variance(l):
   m = mean(l)
-  return sum(map(lambda x: (x-m)**2, l))/len(l)
+  return sum([(x-m)**2 for x in l])/len(l)
 
 def stddev(l):
   return sqrt(variance(l))
